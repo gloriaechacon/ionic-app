e350bc66e8158645bc842a332f1cf6da33f5bc5d
DESKTOP-NKOI6VL\54112 <gloecahcon@gmail.com>
2023-01-09 12:04:45 -0300

    ioni

---
+++ src/app/cities/cities.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import{HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-cities',
+  templateUrl: './cities.page.html',
+  styleUrls: ['./cities.page.scss'],
+})
+export class CitiesPage implements OnInit {
+  cities: any = [];
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getCities().subscribe(res=>{
+      console.log("Res",res)
+      this.cities=res;
+    })
+  }
+
+  getCities(){
+    return this.http
+    .get("assets/files/cities.json")
+    .pipe(
+      map((res:any)=>{
+        return res.data;
+      })
+    )
+  }
+
+}
---
+++ src/app/customers/customers.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import{HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.page.html',
+  styleUrls: ['./customers.page.scss'],
+})
+export class CustomersPage implements OnInit {
+
+  users: any = [];
+  permission: boolean | undefined;
+  constructor(
+    private router: Router,
+    private http: HttpClient
+    ) { }
+
+  ngOnInit() {
+    this.permission=false;
+    //this.permission=true;
+    this.getUsers().subscribe(res=>{
+      console.log("Res",res)
+      this.users=res;
+    })
+  }
+
+  goToDetail(){
+    this.router.navigate(['/details'])
+  }
+
+  getUsers(){
+    return this.http
+    .get("assets/files/customers.json")
+    .pipe(
+      map((res:any)=>{
+        return res.data;
+      })
+    )
+  }
+
+}
